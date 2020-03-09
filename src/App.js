cd57f5507f7d07cde0ed0eb25aa18418399d687e
Joaquin Nader <joaquinnader@gmail.com>
2020-03-09 16:32:28 -0300

    mas cosas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import Card from './Card';
+import React from "react";
+import Card from "./Card";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <Card />
-  );
+  const titles = ["Card", "Pepito", "Juancito", "Picaron", "Etc, etc"];
+
+  return titles.map(function(title) {
+    return <Card key={title} title={title} />;
+  });
 }
 
 export default App;
